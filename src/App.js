29067aa167d9d8416e3d6f38a276604e591f5313
sming0112 <sming0112@gmail.com>
2022-09-03 02:00:40 +0900

    feat: header 레이아웃 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import Router from './router';
 
 function App() {
-    return <ThemeProvider theme={theme}></ThemeProvider>;
+    return (
+        <ThemeProvider theme={theme}>
+            <Router />
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './styles/global';
+import Main from './pages/client/main/main';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <GlobalStyle />
             <Routes>
-                <Route path="/" element={<></>} />
+                <Route path="/" element={<Main />} />
             </Routes>
         </BrowserRouter>
     );
