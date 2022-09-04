9d0e62880639ca68dbcfef90dc1efcea300ef49e
sming0112 <sming0112@gmail.com>
2022-09-05 02:00:57 +0900

    feat: header 구현완료 & ClientRoute 처리

--- src/pages/client/main/main.js
+++ src/pages/client/main/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Footer from '../../../components/client/footer/footer';
 
 const Main = () => {
     return (
         <>
             <div>Main</div>
-            <Footer />
         </>
     );
 };
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import AdminLoginPage from './pages/admin/loginPage';
 import AdminProdListPage from './pages/admin/prodListPage';
 import AdminProdPage from './pages/admin/prodPage';
 import GlobalStyle from './styles/global';
+import ClientRoute from './utils/ClientRoute';
 import AdminRoute from './utils/adminRoute';
 import Main from './pages/client/main/main';
 
@@ -12,7 +13,9 @@ const Router = () => {
             <BrowserRouter>
                 <GlobalStyle />
                 <Routes>
-                    <Route path="/" element={<Main />} />
+                    <Route element={<ClientRoute />}>
+                        <Route path="/" element={<Main />} />
+                    </Route>
                     <Route element={<AdminRoute />}>
                         <Route path="/admin" element={<AdminLoginPage />} />
                         <Route exact path="/admin/prodList" element={<AdminProdListPage />} />
